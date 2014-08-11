9eed93eac587e5b2c8f440d27f73ed74764cf58f
Jim Jeffers <jim@sumocreations.com>
2014-08-11 12:03:41 -0700

    Updated version to 0.1.6

--- lib/confinicky/version.rb
+++ lib/confinicky/version.rb
@@ -1,8 +1,11 @@
 module Confinicky
+  ##
+  # Defines the version of the gem in an internal manner so that
+  # users of the gem can query it at run time.
   module Version
     MAJOR = 0
     MINOR = 1
-    PATCH = 5
+    PATCH = 6
     BUILD = ''
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
