df159712ce56de980dec5c78fceeab29b3b26973
Jim Jeffers <jim@sumocreations.com>
2014-08-10 17:14:56 -0700

    Hardcoded gem version for the time being.

--- test/test_confinicky.rb
+++ test/test_confinicky.rb
@@ -1,7 +1,9 @@
 require 'helper'
 
 class TestConfinicky < MiniTest::Unit::Test
+
   def test_something_for_real
     flunk "hey buddy, you should probably rename this file and start testing for real"
   end
+
 end
