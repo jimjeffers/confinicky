1dcc8766cd865ceea4508af48f6cdca73790b61f
Jim Jeffers <jim@sumocreations.com>
2014-08-23 23:48:24 -0700

    Updated remove command to utilize controllers.

--- lib/confinicky/commands/remove.rb
+++ lib/confinicky/commands/remove.rb
@@ -1,33 +1,47 @@
 command :remove do |c|
   c.syntax = 'cfy remove'
-  c.summary = 'Removes an environment variable in your configuration file.'
+  c.summary = 'Removes an alias or environment variable in your configuration.'
   c.description = ''
-  c.example 'description', 'cfy remove MY_VAR'
+  c.example 'Remove an environment variable', 'cfy remove export MY_VAR'
+  c.example 'Remove an alias', 'cfy remove alias my_alias'
 
   c.action do |args, options|
-    if Confinicky::ShellFile.has_path?
-      say_error "Please set '#{Confinicky::FILE_PATH_VAR}' to point to your local configuration file."
-      puts "Try running 'cfy use' for more info."
+
+    # Abort if not yet setup.
+    if !Confinicky::ConfigurationFile.setup?
+      say_error "Confinicky's configuration is not valid or has not been setup."
+      puts "Try running 'cfy setup'."
       abort
     end
 
-    shell_file = Confinicky::ShellFile.new
+    # Abort if missing arguments.
+    if args.length < 2
+      say_error "You must supply an expression such as: `cfy remove export MY_VAR`"
+      abort
+    end
 
-    duplicate_count = shell_file.find_duplicates.length
-    if duplicate_count > 0
-      say_error "Your configuration cannot be managed because it currently has duplicate export statements."
+    # Use the appropriate command group controller.
+    command, expression = args[0], args[1]
+    command_group = Confinicky::Controllers::Exports.new if command == Confinicky::Arguments::ENVIRONMENT
+    command_group = Confinicky::Controllers::Aliases.new if command == Confinicky::Arguments::ALIAS
+
+    # Abort if duplicate commands have been found.
+    if command_group.duplicates.length > 0
+      say_error "Your configuration cannot be managed because it currently has duplicate statements."
       puts "You must run 'cfy clean' before you can manage your configuration."
       abort
     end
 
-    if shell_file.remove!(args.first)
+    # Set the variable and save changes to disk.
+    if command_group.remove!(expression) and command_group.save!
       say_ok "Successfully removed '#{args.first}'."
-      puts "Open a new terminal/shell window for this change to take affect."
-      shell_file.write!
+      puts "Open a new terminal/shell window for these changes to take affect."
     else
-      say_error "Could not remove '#{args.first}' please double check to ensure you used the appropriate syntax."
+      say_error "Could not remove '#{expression}' please double check to ensure you used the appropriate syntax."
     end
+
   end
+
 end
 
 alias_command :rm, :remove
