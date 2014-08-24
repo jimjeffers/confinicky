c5e86ca3f69aa8c1c1cea9e3368bba55b887f754
Jim Jeffers <jim@sumocreations.com>
2014-08-23 23:24:30 -0700

    Rewrote testing library.

---
+++ test/test_controllers.rb
@@ -0,0 +1,64 @@
+require 'fileutils'
+require 'helper'
+
+class TestCommandGroups < MiniTest::Test
+
+  def setup
+    Confinicky::ConfigurationFile.force_config!({
+      files: {
+        aliases: "test/sample_bash_file.sh.tmp",
+        env: "test/sample_bash_file.sh.tmp"
+      }
+    })
+    @aliases = Confinicky::Controllers::Aliases.new
+    @exports = Confinicky::Controllers::Exports.new
+    @less='\'-i -N -w  -z-4 -g -e -M -X -F -R -P%t?f%f \
+:stdin .?pb%pb\%:?lbLine %lb:?bbByte %bb:-...\''
+  end
+
+  def test_clean_file
+    @exports.clean!
+    assert_equal 17, @exports.length
+  end
+
+  def test_shell_file_duplicates
+    assert_equal 1, @exports.duplicates.length
+  end
+
+  def test_add_var
+    @exports.set!("NEW_VAR=12345")
+    assert_equal 19, @exports.length
+  end
+
+  def test_add_alias
+    @aliases.set!("home=cd ~")
+    assert_equal 33, @aliases.length
+  end
+
+  def test_remove_var
+    @exports.remove!("PATH")
+    assert_equal 16, @exports.length
+  end
+
+  def test_remove_alias
+    @aliases.remove!("debug")
+    assert_equal 31, @aliases.length
+  end
+
+  def test_add_var_should_not_duplicate
+    @exports.set!("NEW_VAR=12345")
+    @exports.set!("NEW_VAR=123456")
+    assert_equal 19, @exports.length
+  end
+
+  def test_whitespace
+    @exports.set!("NEW_STRING=A String")
+    assert_equal "\'A String\'", @exports.find(query: "NEW_STRING")[:value]
+  end
+
+  def test_multiline_statements
+    assert_equal false, @exports.find(query: "LESS")[:value].nil?
+    assert_equal @less, @exports.find(query: "LESS")[:value]
+  end
+
+end
--- test/test_shell_file_parsing.rb
+++ test/test_shell_file_parsing.rb
@@ -1,13 +1,9 @@
 require 'helper'
 
-class TestConfinicky < MiniTest::Test
+class TestShellFileParsing < MiniTest::Test
 
   def setup
     @shell_file = Confinicky::ShellFile.new(file_path: 'test/sample_bash_file.sh')
-  end
-
-  def test_shell_file_duplicates
-    assert_equal 1, @shell_file.find_duplicates.length
   end
 
   def test_exports_without_assignments
