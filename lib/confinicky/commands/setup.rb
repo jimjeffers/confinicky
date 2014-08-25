0842f9c666378ff086ac543ed89b046fdbc9402d
Jim Jeffers <jim@sumocreations.com>
2014-08-24 21:45:12 -0700

    Improved setup command so that it actually creates files if they don't already exist.

--- lib/confinicky/commands/setup.rb
+++ lib/confinicky/commands/setup.rb
@@ -14,12 +14,21 @@ command :setup do |c|
       aliases: "\nWhat file do you want to store your aliases in?\n(leave blank to keep current)"
     }.each do |key, question|
       Confinicky::ConfigurationFile.set_path_for_key(path: ask(question), key: key)
-      say_ok "Using: #{Confinicky::ConfigurationFile.path_for_key(key: key)}"
+      path = Confinicky::ConfigurationFile.path_for_key(key: key)
+      if !File.exists?(path)
+        File.open(path, "w"){|f| f.write "##{key.upcase}\n"}
+        say_ok "Created file at: #{path}"
+      else
+        say_ok "Using: #{Confinicky::ConfigurationFile.path_for_key(key: key)}"
+      end
     end
 
     Confinicky::ConfigurationFile.write!
 
-    say_ok "\nWrote the following configuration to:\n#{Confinicky::ConfigurationFile::PATH}"
-    puts Confinicky::ConfigurationFile.table
+    color "\nWrote the following configuration to:", :bold
+    puts "#{Confinicky::ConfigurationFile::PATH}"
+    color "\nBe sure to add the following lines to your ~/.bash_profile or ~/.bashrc files:", :bold
+    color ". #{Confinicky::ConfigurationFile.path_for_key(key: :env)}", :black, :on_green
+    color ". #{Confinicky::ConfigurationFile.path_for_key(key: :aliases)}", :black, :on_green
   end
 end
